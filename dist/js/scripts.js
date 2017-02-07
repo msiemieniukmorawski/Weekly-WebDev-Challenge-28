71cad79ec5ae3b7e73712fab40ec9d2b1c8bcbd1
MS-M <m.siemieniukmorawski@gmail.com>
2017-02-07 20:42:36 +0100

    Finish fifth section & add img for section & fix bug fourth section in small devices

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -2,5 +2,5 @@ jQuery(".first-section").fitText(.47);
 jQuery(".second-section").fitText(.43);
 jQuery(".third-section").fitText(.575);
 jQuery(".fourth-section").fitText(.36);
-jQuery(".fifth-section").fitText(.50);
-jQuery(".sixth-section").fitText(.47);
+jQuery(".fifth-section").fitText(.456);
+jQuery(".sixth-section").fitText(.485);
